197ab50e9b504fd1ed547600a7387966a6db0111
cao <2050997163@qq.com>
2024-05-16 00:59:43 +0800

    add nacos

--- usr_srv/initialize/config.go
+++ usr_srv/initialize/config.go
@@ -1,7 +1,12 @@
 package initialize
 
 import (
+	"encoding/json"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 	"mxshop_srvs/usr_srv/global"
 )
 
@@ -19,11 +24,57 @@ func InitConfig() {
 	}
 	v := viper.New()
 	v.SetConfigFile(configFileName)
-
 	if err := v.ReadInConfig(); err != nil {
 		panic(err)
 	}
-	if err := v.Unmarshal(&global.ServerConfig); err != nil {
+
+	if err := v.Unmarshal(&global.NacosConfig); err != nil {
 		panic(err)
 	}
+
+	zap.S().Infof("配置信息: %v", global.NacosConfig)
+
+	serverConfigs := []constant.ServerConfig{
+		{
+			IpAddr:      global.NacosConfig.Host,
+			ContextPath: "/nacos",
+			Port:        uint64(global.NacosConfig.Port),
+			Scheme:      "http",
+		},
+	}
+
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         global.NacosConfig.Namespace, // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
+		LogLevel:            "debug",
+	}
+
+	// 创建动态配置客户端
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": serverConfigs,
+		"clientConfig":  clientConfig,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(content)
+
+	err = json.Unmarshal([]byte(content), &global.ServerConfig)
+	if err != nil {
+		zap.S().Fatalf("读取nacos配置失败:%s", err.Error())
+	}
+	//fmt.Println(serverConfig)
+	zap.S().Info(global.ServerConfig)
 }
